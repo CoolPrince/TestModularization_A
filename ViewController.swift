57628e59e28fe5858b0e58b72535269e30cff62c
CoolPrince <cuchch@163.com>
2019-03-07 13:16:16 +0800

    在Development Pods中的TestModularization_A中的ViewController中设置背景色

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Pods-TestModularization_A_Example
+//
+//  Created by cuicc on 2019/3/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        self.view.backgroundColor = UIColor.blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
