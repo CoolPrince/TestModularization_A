c63aeb79ca994df527df5d979449dba059afffa9
CoolPrince <cuchch@163.com>
2019-03-08 14:03:51 +0800

    6

--- TestModularization_A/Classes/Target_TestModularization_A.swift
+++ TestModularization_A/Classes/Target_TestModularization_A.swift
@@ -16,6 +16,14 @@ import UIKit
         let aViewController = ViewController()
         return aViewController
     }
+    @objc func Action_Extension_ViewController_Storyboard(_ params:NSDictionary) -> UIViewController {
+        if let callback = params["callback"] as? (String) -> Void {
+            callback("Action_Extension_ViewController_Storyboard success")
+        }
+
+        let aViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        return aViewController!
+    }
 
     @objc func Action_Category_ViewController(_ params:NSDictionary) -> UIViewController {
 
