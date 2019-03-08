7701bdf9a455630c0b579010b2b287fc8e93f60b
CoolPrince <cuchch@163.com>
2019-03-08 13:35:07 +0800

    5

---
+++ TestModularization_A/Classes/Target_TestModularization_A.swift
@@ -0,0 +1,34 @@
+//
+//  Target_TestModularization_A.swift
+//  Pods-TestModularization_A_Example
+//
+//  Created by cuicc on 2019/3/8.
+//
+
+import UIKit
+
+@objc class Target_TestModularization_A: NSObject {
+    @objc func Action_Extension_ViewController(_ params:NSDictionary) -> UIViewController {
+        if let callback = params["callback"] as? (String) -> Void {
+            callback("Action_Extension_ViewController success")
+        }
+
+        let aViewController = ViewController()
+        return aViewController
+    }
+
+    @objc func Action_Category_ViewController(_ params:NSDictionary) -> UIViewController {
+
+        if let block = params["callback"] {
+
+            typealias CallbackType = @convention(block) (NSString) -> Void
+            let blockPtr = UnsafeRawPointer(Unmanaged<AnyObject>.passUnretained(block as AnyObject).toOpaque())
+            let callback = unsafeBitCast(blockPtr, to: CallbackType.self)
+
+            callback("Action_Category_ViewController success")
+        }
+
+        let aViewController = ViewController()
+        return aViewController
+    }
+}
